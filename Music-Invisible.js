c11dc52bd8be0c309bc6d5e112af3fd7ddba5e1a
Mr_Invisible_Boy <101985120+MR-INVISIBLEBOY@users.noreply.github.com>
2022-04-04 07:59:27 -0700

    Create Music-Invisible.js

---
+++ Music-Invisible.js
@@ -0,0 +1,16 @@
+from TheExpres import run clone#
+from import java , python , linux , music , bot , invisible , 
+  run clone int(str(invisible_music))).get 
+        run clone(1000mbs.>/?)}
+                  opner_>l?
+                  { 
+         musicbot(invisible).get import {.get , java , python , })).....
+        musicbot.int(str(legendhacker_iin_invisibleboy_baap_).int<>+}].output(invisible).)))
+        
+        
+        #clonetharepo_ .int.str{int}.pettenor/user./hacknot
+(send.messesge.(89765456434)-[buttonurl://github.com/MR-INVISIBLEBOY/MUSICALBOT-INVISIBLE].oit
+                              get from run utlis.import
+                              get from fun #clone 
+                              get import.fun /?
+                              

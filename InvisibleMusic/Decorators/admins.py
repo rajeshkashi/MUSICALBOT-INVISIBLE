113c9eb370670d09beb9bc5597947474c8f6af30
【★༒✞︎©༒ʟɛɢɛռɖ✘-թπο】ρɾαωαรɦ-Malviya <87700009+LEGEND-LX@users.noreply.github.com>
2022-04-13 09:32:09 -0700

    Update admins.py

--- InvisibleMusic/Decorators/admins.py
+++ InvisibleMusic/Decorators/admins.py
@@ -1,11 +1,11 @@
 from typing import Dict, List, Union
 
-from Yukki import SUDOERS, app
-from Yukki.Database import (_get_authusers, add_nonadmin_chat,
+from InvisibleMusic import SUDOERS, app
+from InvisibleMusic.Database import (_get_authusers, add_nonadmin_chat,
                             delete_authuser, get_authuser, get_authuser_count,
                             get_authuser_names, is_nonadmin_chat,
                             remove_nonadmin_chat, save_authuser)
-from Yukki.Utilities.changers import int_to_alpha
+from InvisibleMusic.Utilities.changers import int_to_alpha
 
 
 def AdminRightsCheck(mystic):

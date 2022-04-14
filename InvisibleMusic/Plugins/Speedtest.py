e92a3505fbc6bcec7a981bd0c89ee1c79bd64b72
【★༒✞︎©༒ʟɛɢɛռɖ✘-թπο】ρɾαωαรɦ-Malviya <87700009+LEGEND-LX@users.noreply.github.com>
2022-04-14 08:31:30 -0700

    Update Speedtest.py

--- InvisibleMusic/Plugins/Speedtest.py
+++ InvisibleMusic/Plugins/Speedtest.py
@@ -5,7 +5,7 @@ import wget
 from pyrogram import Client, filters
 from pyrogram.types import Message
 
-from Yukki import BOT_ID, SUDOERS, app
+from InvisibleMusic import BOT_ID, SUDOERS, app
 
 __MODULE__ = "Speedtest"
 __HELP__ = """

54e331707cea50d87048bdd48889f8a07fea4a88
【★༒✞︎©༒ʟɛɢɛռɖ✘-թπο】ρɾαωαรɦ-Malviya <87700009+LEGEND-LX@users.noreply.github.com>
2022-04-14 08:27:34 -0700

    Update Playlist.py

--- InvisibleMusic/Plugins/Playlist.py
+++ InvisibleMusic/Plugins/Playlist.py
@@ -3,14 +3,14 @@ from pyrogram.types import (InlineKeyboardButton, InlineKeyboardMarkup,
                             KeyboardButton, Message, ReplyKeyboardMarkup,
                             ReplyKeyboardRemove)
 
-from Yukki import BOT_ID, BOT_USERNAME, MUSIC_BOT_NAME, SUDOERS, app, db_mem
-from Yukki.Database import (_get_playlists, delete_playlist, get_playlist,
+from InvisibleMusic import BOT_ID, BOT_USERNAME, MUSIC_BOT_NAME, SUDOERS, app, db_mem
+from InvisibleMusic.Database import (_get_playlists, delete_playlist, get_playlist,
                             get_playlist_names, save_playlist)
-from Yukki.Decorators.admins import AdminRightsCheck
-from Yukki.Decorators.assistant import AssistantAdd
-from Yukki.Decorators.checker import checker, checkerCB
-from Yukki.Decorators.permission import PermissionCheck
-from Yukki.Inline import (add_genre_markup, check_genre_markup, check_markup,
+from InvisibleMusic.Decorators.admins import AdminRightsCheck
+from InvisibleMusic.Decorators.assistant import AssistantAdd
+from InvisibleMusic.Decorators.checker import checker, checkerCB
+from InvisibleMusic.Decorators.permission import PermissionCheck
+from InvisibleMusic.Inline import (add_genre_markup, check_genre_markup, check_markup,
                           delete_playlist_markuup, download_markup,
                           others_markup, play_genre_playlist, playlist_markup,
                           third_playlist_markup)
